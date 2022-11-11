db4f0f104ef6fb7692a0debe820a9b5ae4f4be70
jamesdgalbraith <galbraithenator@gmail.com>
2022-11-11 14:04:55 +0000

    updated Dfam extractor to preserve non-Dfam sequences

--- scripts/Dfam_extractor.py
+++ scripts/Dfam_extractor.py
@@ -22,6 +22,7 @@ if(exists(args.directory) == False):
 
 # name of out file
 out_library=args.directory+'/'+sub('.*/', '', args.library)
+nondfam_library=args.directory+'/non-dfam.'+sub('.*/', '', args.library)
 
 # write Dfam pseudo-curated sequences to out_library
 # sequence headers beginning with DR and having digits between "DR" and "#" treated as as Dfam
@@ -31,3 +32,13 @@ with open(out_library, 'w') as out_handle:
       if record.name.startswith("DR"):
         if sub('DR', '', sub('#.*', '', record.name)).isdigit():
           SeqIO.write(record, out_handle, "fasta")
+
+# write non-Dfam sequences to out_library
+# sequence headers beginning with DR and having digits between "DR" and "#" treated as as Dfam
+with open(nondfam_library, 'w') as out_handle:
+  with open(args.library, 'r') as in_handle:
+    for record in SeqIO.parse(in_handle, "fasta"):
+      if record.name.startswith("DR") is False:
+        SeqIO.write(record, out_handle, "fasta")
+      elif sub('DR', '', sub('#.*', '', record.name)).isdigit() is False:
+        SeqIO.write(record, out_handle, "fasta")
