e7c74f8a5f925833ce1a32f6617302075c822e2a
jamesdgalbraith <galbraithenator@gmail.com>
2022-11-17 15:25:41 +0000

    Added more good+bad domains

--- scripts/domain_plotter.R
+++ scripts/domain_plotter.R
@@ -1,20 +1,20 @@
 library(tidyverse)
 
-in_seq <- "seq/Ceratotherium_simum_cottoni-families.fa"
-data_dir <- "TS_Ceratotherium_simum_cottoni-families.fa_8840/"
+in_seq <- "seq/Chiloscyllium_punctatum-families.fa"
+data_dir <- "TS_Chiloscyllium_punctatum-families.fa_5438/"
 seq_name <- sub(".*\\/", "", in_seq)
 
 og_seq<-Biostrings::readDNAStringSet(in_seq)
 og_tibble <- tibble(seqnames = names(og_seq), width = width(og_seq)) %>%
   mutate(raw_names = sub("#.*", "", seqnames))
 
-chimeric_seq<-Biostrings::readDNAStringSet("TS_Ceratotherium_simum_cottoni-families.fa_8840/chimeras/chimeric_Ceratotherium_simum_cottoni-families.fa")
-unacceptable_seq<-Biostrings::readDNAStringSet("TS_Ceratotherium_simum_cottoni-families.fa_8840/chimeras/questionable_Ceratotherium_simum_cottoni-families.fa")
+chimeric_seq<-Biostrings::readDNAStringSet(paste0(data_dir, "/chimeras/chimeric_", seq_name))
+unacceptable_seq <-Biostrings::readDNAStringSet(paste0(data_dir, "/chimeras/questionable_", seq_name))
 chimeric_seq <- c(chimeric_seq, unacceptable_seq)
 chimeric_tibble <- tibble(seqnames = names(chimeric_seq), width = width(chimeric_seq)) %>%
   mutate(raw_names = sub("#.*", "", seqnames))
 
-rps_blast_out <- read_tsv(file = "TS_Ceratotherium_simum_cottoni-families.fa_8840/chimeras/Ceratotherium_simum_cottoni-families.fa.rps.out",
+rps_blast_out <- read_tsv(file = paste0(data_dir, "chimeras/", seq_name, ".rps.out"),
                           col_names = c("seqnames", "qstart", "qend", "qlen", "sseqid", "sstart", "send", "slen",
                                         "pident", "length", "mismatch", "gapopen", "evalue", "bitscore", "qcovs", "stitle"),
                           show_col_types = FALSE) %>%
@@ -24,7 +24,7 @@ rps_blast_out <- read_tsv(file = "TS_Ceratotherium_simum_cottoni-families.fa_884
 acceptable_domains <- rbind(read_tsv("data/acceptable_domains.tsv"), read_tsv("data/additional_domains.tsv", col_select = c(ref, abbrev)))
 unacceptable_domains <- rbind(read_tsv("data/unacceptable_domains.tsv", col_select = c(ref, abbrev)))
 
-chimeric_tibble <- rps_blast_out[sub("#.*", "", rps_blast_out$seqnames) %in% chimeric_tibble$raw_names,]
+chimeric_tibble <- rps_blast_out[sub("#.*", "", rps_blast_out$seqnames) %in% c(chimeric_tibble$raw_names),]
 
 unacceptable <- chimeric_tibble[!chimeric_tibble$ref %in% acceptable_domains$ref,]$seqnames
 
@@ -33,8 +33,9 @@ chimeric_tibble <- rps_blast_out[sub("#.*", "", rps_blast_out$seqnames) %in% sub
 chimeric_tibble <- chimeric_tibble %>%
   mutate(missing = ifelse(ref %in% acceptable_domains$ref, FALSE, TRUE))
 chimeric_names <- base::unique(chimeric_tibble$seqnames)
+chimeric_names
 
-i=13
+i=4
 
 to_plot <- chimeric_tibble[chimeric_tibble$seqnames == chimeric_names[i],] %>% arrange(qstart)
 to_plot$y = 1:nrow(to_plot)
@@ -46,8 +47,7 @@ ggplot(to_plot) +
   ggtitle(to_plot$seqnames[1])
 to_plot %>%
   filter(!ref %in% unacceptable_domains$ref, !ref %in% acceptable_domains$ref) %>%
-  # dplyr::select(ref, abbrev) %>%
-  # base::unique() %>%
-  # mutate(organism = "Ceratotherium_simum_cottoni") %>%
-  # write_tsv("TS_Ceratotherium_simum_cottoni-families.fa_8840/unacceptable_domains.tsv")
-NULL
+  dplyr::select(ref, abbrev) %>%
+  base::unique() %>%
+  mutate(organism = "Chiloscyllium_punctatum") %>%
+  write_tsv("new_unacceptable_domains.tsv")
